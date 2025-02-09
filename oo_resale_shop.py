def8b6f2e62fe4ccb4e8e9db3b7583901183ffd3
HarlowMoran <hmoran72@smith.edu>
2025-02-09 18:36:47 -0500

    oo_resale_shop.py Commit

--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,6 +1,9 @@
+from computer import *
+
 class ResaleShop:
 
     # What attributes will it need?
+    inventory = []
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
@@ -8,3 +11,16 @@ class ResaleShop:
         pass # You'll remove this when you fill out your constructor
 
     # What methods will you need?
+    #I need the ability to add a computer to the inventory, and return it's computer ID
+    def buy(self, new_computer, description, processor_type, hard_drive_capacity, memory, operating_system, year_made, price):
+            self.description = description
+            self.processor_type = processor_type
+            self.hard_drive_capacity = hard_drive_capacity
+            self.memory = memory
+            self.operating_system = operating_system
+            self.year_made = year_made
+            self.price = price
+            new_computer = Computer(self, description, processor_type, hard_drive_capacity, memory, operating_system, year_made, price)
+            return new_computer
+    #I need the ability to edit this inventory to add the new computer
+    inventory.append(buy)
